911b7d3b0601212e1f1bd1da34e3ee39aa4cf701
Atharva Pandey <atharvajava@gmail.com>
2019-02-07 17:34:03 +0100

    Base Complete

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,37 @@
 import React from "react"
-import Header from "../components/shared/Header"
 import BaseLayout from "../components/layouts/BaseLayout"
+import axios from "axios"
 
-class Index extends React.Component {
+class Index extends React.Component {    
+
+    static async getInitialProps(){
+        let userData={}
+        try {
+            const response = await axios.get("https://jsonplaceholder.typicode.com/todos/1")
+            userData=response.data
+        } catch (error) {
+            console.log(err)
+        }
+       
+       return {initialData:[1,2,3,4], userData}
+    }
+
+    constructor(){
+        super();
+        this.state = {
+            title : "I am Index page"
+        }
+    }
+
     render(){
+        const {title} = this.state
+        const {userData , initialData } = this.props
         return (
             <BaseLayout>
                 <h1>Index Page class</h1>
+                <h2>{title}</h2>
+                <h2>{userData.title}</h2>
+                <button onClick={()=>{this.setState({title:"Changed Title"})}}>Change Title</button>
             </BaseLayout>
         )
     }
--- pages/portfolios.js
+++ pages/portfolios.js
@@ -1,11 +1,42 @@
 import React from "react"
 import BaseLayout from "../components/layouts/BaseLayout"
+import axios from "axios"
+import {Link} from '../routes'
 
 class Portfolios extends React.Component {
+
+    static async getInitialProps(){
+        let posts= []
+        try {
+            const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
+            posts= response.data
+        } catch (error) {
+            console.log(error)
+        }
+
+        return {posts : posts.splice(0,10)}
+    }
+
+    renderPosts(posts) {
+        return posts.map((post,index)=>{
+            return (
+                <li key={index}>
+                     <Link route={`/portfoliodetail/${post.id}`} >
+                        <a >{post.title}</a>
+                    </Link>
+                </li>
+            )
+        })
+    }
+
     render(){
+        const {posts} = this.props
         return (
             <BaseLayout>
                 <h1>Portfolios Page class</h1>
+                <ul>
+                    {this.renderPosts(posts)}
+                </ul>
             </BaseLayout>
         )
     }
